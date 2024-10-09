784211943bea5ae157821c02517a4372b02916c3
akshay-dhawle-135 <akshay.dhawle@xplortechnologies.com>
2024-10-09 19:32:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports.handler = async (event) => {
+  const DB_HOST = process.env.DB_HOST; 
+  const DB_USER_NAME = process.env.DB_USER_NAME; 
+  const DB_PASSWORD = process.env.DB_PASSWORD; 
+
+  console.log('DB_HOST', DB_HOST);
+  console.log('DB_USER_NAME', DB_USER_NAME);
+  console.log('DB_PASSWORD', DB_PASSWORD);
+  console.log('DB_PASSWORD', DB_PASSWORD);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello World!',
+      },
+      null,
+      2
+    ),
+  };
+};
